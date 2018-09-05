7d459b6fa13ea0ada1349f190c693707e106ba88
kunal097 <kunalsharma19997@gmail.com>
2018-09-05 13:50:53 +0530

    initial commit

---
+++ src/management/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+	username = models.CharField(max_length=20)
+	password = models.CharField(max_length=15)
+	is_authorised = models.BooleanField()
+
+
+
+
+class criminal(models.Model):
+	pass
+
+
+class record(models.Model):
+	pass

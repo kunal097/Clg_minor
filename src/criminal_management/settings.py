96b9d50b924acd27631f284d886136d536f84835
kunal097 <kunalsharma19997@gmail.com>
2018-10-11 06:36:10 +0530

    add frontend

--- src/criminal_management/settings.py
+++ src/criminal_management/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+SERVER_EMAIL = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'passowrd'
+EMAIL_HOST_USER = SERVER_EMAIL
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
